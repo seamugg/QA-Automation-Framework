afa8c5fc2b7b04c1611789cce6d1da63c768643a
bautmus <seamugg@yahoo.com>
2022-11-14 12:44:24 -0500

    update

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -3,20 +3,25 @@ import org.jetbrains.annotations.NotNull;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.safari.SafariDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.DataProvider;
+import org.testng.annotations.*;
 
+import java.net.MalformedURLException;
+import java.net.URI;
 import java.time.Duration;
 
 public class BaseTest {
 
     WebDriver driver;
     String url;
+    Actions actions;
 
 
 
@@ -39,18 +44,52 @@ public class BaseTest {
         };
     }
 
-    @BeforeMethod
-    public void launchBrowser() {
-       // driver = new ChromeDriver();
-
-        System.setProperty("webdriver.gecko.driver","geckodriver.exe");
-        driver = new FirefoxDriver();
-        Actions actions = new Actions(driver);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+ //   @BeforeClass
+//    public void launchBrowser() {
+//       // driver = new ChromeDriver();
+//
+//        System.setProperty("webdriver.gecko.driver","geckodriver.exe");
+//        driver = new FirefoxDriver();
+//        Actions actions = new Actions(driver);
+//        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+//        driver.manage().window().maximize();
+//        url = "https://bbb.testpro.io/#!/home";
+//        driver.get(url);
+//
+//    }
+    @BeforeClass
+    @Parameters({"baseURL"})
+    public void launchBrowser(@Optional String baseURL) throws MalformedURLException {
+        if (baseURL == null)
+        baseURL ="https://bbb.testpro.io";
+        driver = pickBrowser("grid-firefox");
         driver.manage().window().maximize();
-        url = "https://bbb.testpro.io/#!/home";
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        url = baseURL;
         driver.get(url);
+    }
+    public WebDriver pickBrowser(@NotNull String browser) throws MalformedURLException {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        String gridURL = "http://192.168.1.74'";
+        switch (browser) {
+            case "firefox":
+                System.setProperty("webdriver.gecko.driver", "geckodriver.exe");
+                return driver = new FirefoxDriver();
+            case "safari":
+                return driver = new SafariDriver();
+            case "grid-safari":
+                capabilities.setCapability("browserName", "safari");
+                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(), capabilities);
+            case "grid-firefox":
+                capabilities.setCapability("browserName", "firefox");
+                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(), capabilities);
+            case "grid-chrome":
+                capabilities.setCapability("browserName", "chrome");
+                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(), capabilities);
+            default:
+                return driver = new ChromeDriver();
 
+        }
     }
 
     public void clickSubmitBtn() {
@@ -80,6 +119,12 @@ public class BaseTest {
         return new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions
                 .visibilityOf(webElementLocator));
     }
+
+    @AfterMethod
+    public void tearDownBrowser() {
+        driver.quit();
+    }
+
 }
 
 
